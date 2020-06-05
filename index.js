7242cc6d21012a67c618a934ec7a0ccf2eb6d74b
Aanand Kainth <aanand@akainth.me>
2020-06-05 00:53:52 -0700

    Literally stashing work for the night

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const http = require("http");
 const fs = require("fs");
 const formidable = require("formidable");
 const request = require("request");
+const WebSocket = require('ws');
+const url = require('url');
 
-http.createServer((req, res) => {
+const wss1 = new WebSocket.Server({ noServer: true });
+
+wss1.on('connection', function connection(ws) {
+    console.debug("new connection");
+});
+
+const server = http.createServer((req, res) => {
     if (req.method.toLowerCase() === "post") {
         const form = formidable();
 
@@ -27,4 +35,18 @@ http.createServer((req, res) => {
     }
     res.statusCode = 404;
     res.end();
-}).listen(5012);
+});
+
+server.on('upgrade', function upgrade(request, socket, head) {
+    const pathname = url.parse(request.url).pathname;
+
+    if (pathname === '/ws') {
+        wss1.handleUpgrade(request, socket, head, function done(ws) {
+            wss1.emit('connection', ws, request);
+        });
+    } else {
+        socket.destroy();
+    }
+});
+
+server.listen(5012);
